9eb1a95be58b5295cd72000a0c3dcf74330228dc
Elvirience <elvirience@mail.ru>
2021-11-01 18:09:24 +0300

    lesson3

---
+++ lesson3/ex2.py
@@ -0,0 +1,11 @@
+def num2(name, nom, born, city, email, number):
+    print(f'Имя: {name}\tФамилия: {nom}\tГод рождения: {born}\tГород: {city}\te-mail: {email}\tТелефон: {number}\n')
+
+
+name = input('Имя: ')
+nom = input('Фамилия: ')
+born = input('Год рождения: ')
+city = input('Город: ')
+email = input('e-mail: ')
+number = input('Телефон: ')
+num2(name, nom, born, city, email, number)
