3cd4e4beafc72fd9299f2e10c3359bc8297a1805
Elvirience <92172008+Elvirience@users.noreply.github.com>
2021-11-08 18:23:37 +0300

    Delete ex4.py

--- lesson4/ex4.py
+++
@@ -1,7 +0,0 @@
-from random import randrange
-
-len_gen = randrange(20)
-gen = [randrange(20) for i in range(len_gen)]
-print(gen)
-new_gen = [el for el in gen if gen.count(el) == 1]
-print(new_gen)
