e47aee7e7810e534e22c7929aa56cd4b84ece2e1
Elvirience <92172008+Elvirience@users.noreply.github.com>
2021-11-12 12:37:43 +0300

    Delete ex2.py

--- lesson5/ex2.py
+++
@@ -1,8 +0,0 @@
-lines = 0
-words = 0
-fout = open('ex2.txt', encoding='UTF-8')
-for i in fout.readlines():
-    print(f"Число слов в строке {lines + 1}: {len(i.split())}")
-    lines += 1
-print(f'Число строк: {lines}')
-fout.close()
