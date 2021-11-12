942727039ba2b6d6fb641e19c9868c006bcd20e9
Elvirience <92172008+Elvirience@users.noreply.github.com>
2021-11-12 08:15:40 +0300

    Delete ex1.py

--- lesson5/ex1.py
+++
@@ -1,6 +0,0 @@
-fin = open('ex1.txt', 'w', encoding='UTF-8')
-data = input('Введите данные для записи в файл или пустую строку: ')
-while data != '':
-    fin.write(data + '\n')
-    data = input('Введите данные для записи в файл или пустую строку: ')
-fin.close()
