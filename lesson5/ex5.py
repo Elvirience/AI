12d7170de983b0f027ce5ae6853f12f89b318a44
Elvirience <92172008+Elvirience@users.noreply.github.com>
2022-03-06 16:52:33 +0300

    Delete ex5.py

--- lesson5/ex5.py
+++
@@ -1,8 +0,0 @@
-i = 0
-sum = 0
-with open('ex5.txt', 'w+', encoding='UTF - 8') as fin:
-    fin.write('5 0 1 2 3 7 9 5 6 81 4 56 1')
-    fin.seek(0)
-    for i in fin.readline().split():
-        sum += int(i)
-    print(sum)
